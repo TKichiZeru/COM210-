8a384b62bbc70edfc21bca1a1f1575e48519d891
TKichiZeru <123252102+TKichiZeru@users.noreply.github.com>
2023-02-12 04:39:56 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,22 +1,20 @@
-
-
-
 import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList; // import the ArrayList class
 
-public class App {
-  
-    
-    
+public class App
+ {
+
+
+
 
 
     public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
-        Scanner myPrices = new Scanner(System.in);
+    Scanner myPrices = new Scanner(System.in);
 
         ArrayList<String> myObjs = new ArrayList<String>();
         ArrayList<Double> myPricess = new ArrayList<Double>();
-       
+
         String items = null;
         double price;
         String[] AmtList = {"First", "Second", "Third"};
@@ -34,13 +32,15 @@ public class App {
                 price = myPrices.nextDouble();
                 myPricess.add(price);
             }
-            }     
-        }    
+            }
+        }
         System.out.println("The Items are: " + myObjs);
         double totalprice = 0.00;
         for (double i : myPricess){
             totalprice += i;
+             
         }
-        System.out.println("The Total Price is: $" + totalprice);      
+        System.out.println("The Total Price is: $" + totalprice);
+        System.out.println("The Average Price is: $" + totalprice/3);
     }
 }
