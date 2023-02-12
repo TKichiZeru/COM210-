6d127d494afc1f8a1e0b2ee5ff94a92c57ff199d
TKichiZeru <123252102+TKichiZeru@users.noreply.github.com>
2023-02-12 04:46:11 -0500

    Update App.java

--- App.java
+++ App.java
@@ -9,30 +9,25 @@ import java.util.ArrayList; // import the ArrayList class
 
 public class App
  {
-
-
-
-
-
     public static void main(String[] args) {
+
     Scanner myObj = new Scanner(System.in);
     Scanner myPrices = new Scanner(System.in);
 
-        ArrayList<String> myObjs = new ArrayList<String>();
-        ArrayList<Double> myPricess = new ArrayList<Double>();
+    ArrayList<String> myObjs = new ArrayList<String>();
+    ArrayList<Double> myPricess = new ArrayList<Double>();
 
-        String items = null;
-        double price;
-        String[] AmtList = {"First", "Second", "Third"};
-
-
-        for (int i = 0; i < 1; i++){
-            //Enter Item and press Enter
-            for(int k = 0; k < 3; k++){
-                System.out.println("Enter " + AmtList[k]+ " Item.");
-                items = myObj.nextLine();
-                myObjs.add(items);
-                //Find TotalPrice
+    String items = null;
+    double price;
+    String[] AmtList = {"First", "Second", "Third"};
+    
+    for (int i = 0; i < 1; i++){
+        //Enter Item and press Enter
+        for(int k = 0; k < 3; k++){
+            System.out.println("Enter " + AmtList[k]+ " Item.");
+            items = myObj.nextLine();
+            myObjs.add(items);
+            //Find TotalPrice
             for (int j = 0; j < 1; j++){
                 System.out.println("Enter Price:");
                 price = myPrices.nextDouble();
@@ -40,12 +35,14 @@ public class App
             }
             }
         }
+
         System.out.println("The Items are: " + myObjs);
         double totalprice = 0.00;
         for (double i : myPricess){
             totalprice += i;
              
         }
+        
         System.out.println("The Total Price is: $" + totalprice);
         System.out.println("The Average Price is: $" + totalprice/3);
     }
